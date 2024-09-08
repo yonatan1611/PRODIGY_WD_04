c164b8f948e620094a34be6018e53f59f2b64110
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2024-09-09 00:54:07 +0300

    home

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className==="nav-menu"){
+        menuBtn.className += "responsive";
+    }else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+window.onscroll = function(){headerShadow()};
+
+function headerShadow(){
+    const navHeader = document.getElementById("header");
+
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 ) {
+        navHeader.style.boxShadow = "0 1px 9px rgba(0,0,0,0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else{
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+}
