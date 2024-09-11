b9f5359b0141d272d44fff17d43d5228cd013647
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2024-09-11 03:34:26 +0300

    contact and footer section

--- js/main.js
+++ js/main.js
@@ -23,3 +23,51 @@ function headerShadow(){
         navHeader.style.lineHeight = "90px";
     }
 }
+
+var typingEffect = new Typed(".typedText",{
+    strings : ["Designer","Video Editer", "Developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+})
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true     
+})
+
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+sr.reveal('.project-box',{interval: 200})
+sr.reveal('.top-header',{})
+
+const srLeft = ScrollReveal({
+origin: 'left',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+const srRight = ScrollReveal({
+origin: 'right',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+
